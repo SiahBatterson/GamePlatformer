120f70dd3b559645bfdbf7c1cbdfa291189231e8
SiahBatterson <siahbatterson@gmail.com>
2021-09-07 13:56:01 -0400

    project files
    these are the project files

---
+++ Tower Platformer/Assets/Movement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float speed = 5;
+    public float momentumthreshold;
+    public float horizontalthreshhold;
+    private int basespeed;
+    private float horizontal;
+    private float vertical;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        basespeed = (int)speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    horizontal = Input.GetAxisRaw("Horizontal");
+    if(horizontal > horizontalthreshhold){horizontal = horizontalthreshhold;}
+    if(horizontal < -horizontalthreshhold){horizontal = -horizontalthreshhold;}
+    if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)){speed += 0.00001f;}
+    if(speed > momentumthreshold){speed = momentumthreshold;}
+    else{speed = basespeed;}
+    if(Input.GetKeyDown(KeyCode.Space)){rb.AddForce(new Vector2(0, 5*speed), ForceMode2D.Impulse);Debug.Log("Jump");}
+    rb.AddForce(new Vector2(horizontal*speed,0));
+    }
+}
