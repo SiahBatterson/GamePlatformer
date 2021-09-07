7764d14219772b0eb63e9154e2d04b9d4d55432d
SiahBatterson <siahbatterson@gmail.com>
2021-09-07 19:17:00 -0400

    Movement

--- Tower Platformer/Assets/Movement.cs
+++ Tower Platformer/Assets/Movement.cs
@@ -6,6 +6,7 @@ public class Movement : MonoBehaviour
 {
     Rigidbody2D rb;
     public float speed = 5;
+    public float jumpheight;
     public float momentumthreshold;
     public float horizontalthreshhold;
     private int basespeed;
@@ -24,10 +25,11 @@ public class Movement : MonoBehaviour
     horizontal = Input.GetAxisRaw("Horizontal");
     if(horizontal > horizontalthreshhold){horizontal = horizontalthreshhold;}
     if(horizontal < -horizontalthreshhold){horizontal = -horizontalthreshhold;}
-    if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)){speed += 0.00001f;}
+    if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)){speed += 0.001f;}
+    else{if(horizontal>0.5){horizontal-=0.5f;}if(horizontal<-0.5){horizontal+=0.5f;}}
     if(speed > momentumthreshold){speed = momentumthreshold;}
     else{speed = basespeed;}
-    if(Input.GetKeyDown(KeyCode.Space)){rb.AddForce(new Vector2(0, 5*speed), ForceMode2D.Impulse);Debug.Log("Jump");}
+    if(Input.GetKeyDown(KeyCode.Space)){rb.AddForce(new Vector2(0, jumpheight*speed), ForceMode2D.Impulse);Debug.Log("Jump");}
     rb.AddForce(new Vector2(horizontal*speed,0));
     }
 }
